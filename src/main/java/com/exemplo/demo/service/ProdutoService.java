d7f811ee2dcaf7ee91a30bdf0ad4ac8fb38222a8
leocassiosilva <leocassiosilva1234@gmail.com>
2020-03-18 22:53:35 -0300

    Commitando novamente

---
+++ src/main/java/com/exemplo/demo/service/ProdutoService.java
@@ -0,0 +1,18 @@
+package com.exemplo.demo.service;
+
+import java.util.List;
+
+import com.exemplo.demo.domain.Produto;
+
+public interface ProdutoService {
+	
+	void salvar(Produto produto); 
+	
+	void editar(Produto produto); 
+	
+	void excluir(Long id);
+	
+	Produto buscarPorId(Long id);
+	
+	List<Produto> buscarTodos();
+}
---
+++ src/main/java/com/exemplo/demo/service/ProdutoServiceImpl.java
@@ -0,0 +1,45 @@
+package com.exemplo.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.exemplo.demo.dao.ProdutoDao;
+import com.exemplo.demo.domain.Produto;
+
+@Service @Transactional(readOnly = false)
+public class ProdutoServiceImpl implements ProdutoService{
+
+	
+	private ProdutoDao dao;
+	
+	
+	@Override
+	public void salvar(Produto produto) {
+		dao.save(produto);
+	}
+
+	@Override
+	public void editar(Produto produto) {
+		dao.update(produto);
+		
+	}
+
+	@Override
+	public void excluir(Long id) {
+		dao.delete(id);
+		
+	}
+
+	@Override @Transactional(readOnly = true)
+	public Produto buscarPorId(Long id) {
+		return dao.findById(id);
+	}
+
+	@Override @Transactional(readOnly = true)
+	public List<Produto> buscarTodos() {
+		return dao.findAll();
+	}
+
+}
